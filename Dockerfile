3badb5589305c8d7be535fe781d9fd0001ff877b
deenu-cse <236301074@gkv.ac.in>
2026-01-19 21:25:31 +0530

    update tessract in code

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,8 @@ FROM python:3.11-slim
 
 WORKDIR /app
 
-# ✅ Required runtime deps for opencv-python-headless
 RUN apt-get update && apt-get install -y \
+    tesseract-ocr \
     libglib2.0-0 \
     libsm6 \
     libxext6 \
@@ -18,5 +18,4 @@ RUN pip install --no-cache-dir -r requirements.txt
 COPY . .
 
 EXPOSE 8000
-
 CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
--- ocr.py
+++ ocr.py
@@ -1,20 +1,17 @@
 import cv2
 import numpy as np
+import pytesseract
 
 def extract_text_from_image_bytes(image_bytes: bytes) -> str:
     np_arr = np.frombuffer(image_bytes, np.uint8)
     img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+
     if img is None:
         return ""
 
     img = cv2.resize(img, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (5,5), 0)
-    thresh = cv2.adaptiveThreshold(
-        gray, 255,
-        cv2.ADAPTIVE_THRESH_MEAN_C,
-        cv2.THRESH_BINARY,
-        15, 3
-    )
 
-    return f"IMAGE_DATA_SHAPE:{thresh.shape}"
+    text = pytesseract.image_to_string(gray)
+    return text
